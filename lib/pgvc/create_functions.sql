d54f86e65d1baf27d4b7e0a08b967b7901274047
Josh Cheek <josh.cheek@gmail.com>
2017-10-21 14:34:05 -0500

    Can make a commit, minus its ancestry

--- lib/pgvc/create_functions.sql
+++ lib/pgvc/create_functions.sql
@@ -107,6 +107,44 @@ create function vc.get_commit(commit_hash character(32)) returns vc.commits as $
 
 
 
+create function vc.save_table
+  ( in  schema_name varchar,
+    in  table_name  varchar,
+    out table_hash  character(32)
+  ) as $$
+  declare row_hashes character(32)[];
+  begin
+    raise WARNING 'schema: %, table: %', schema_name, table_name;
+    execute format('select array_agg(vc_hash) from %s.%s;',
+                   quote_ident(schema_name),
+                   quote_ident(table_name))
+            into row_hashes;
+    row_hashes := coalesce(row_hashes, '{}'); -- use empty array rather than null
+    insert into vc.tables (vc_hash, row_hashes)
+      values (md5(row_hashes::text), row_hashes)
+      returning vc_hash into table_hash;
+  end $$ language plpgsql;
+
+
+
+create function vc.save_branch(in schema_name varchar) returns character(32) as $$
+  declare
+    db vc.databases;
+    table_name varchar;
+  begin
+    db.table_hashes := (
+      select hstore(
+        array_agg(name),
+        array_agg(vc.save_table(schema_name, name))
+      ) from vc.tracked_tables);
+    db.vc_hash := md5(db.table_hashes::text);
+    insert into vc.databases select db.*;
+    return db.vc_hash;
+  end
+  $$ language plpgsql;
+
+
+
 create function vc.create_commit
   ( in summary     vc.commits.summary%TYPE,
     in description vc.commits.description%TYPE,
@@ -115,15 +153,17 @@ create function vc.create_commit
   ) returns vc.commits as $$
   declare
     cmt vc.commits;
+    branch vc.branches;
   begin
+    branch          := vc.get_branch(user_id);
     cmt.summary     := summary;
     cmt.description := description;
     cmt.user_id     := user_id;
     cmt.created_at  := created_at;
-    cmt.db_hash     := '................................';
+    cmt.db_hash     := vc.save_branch(branch.schema_name);
     cmt.vc_hash     := vc.calculate_commit_hash(cmt);
-    raise WARNING 'COMMITTING: %', cmt;
     insert into vc.commits select cmt.*;
+    update vc.branches set commit_hash = cmt.vc_hash where id = branch.id;
     return cmt;
   end
   $$ language plpgsql;
