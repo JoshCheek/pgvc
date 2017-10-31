2cc524163cff42c204f00dc3ad92e0fe1304f928
Josh Cheek <josh.cheek@gmail.com>
2017-10-31 09:50:31 -0500

    Update the examples

--- example.rb
+++ example.rb
@@ -1,120 +1,85 @@
 # Reset the database
-require 'pg'
-PG.connect(dbname: 'postgres')
-  .tap { |db| db.exec 'drop database if exists pgvc_testing' }
-  .tap { |db| db.exec 'create database pgvc_testing' }
-db = PG.connect dbname: 'pgvc_testing'
+  require 'pg'
+  PG.connect(dbname: 'postgres')
+    .tap { |db| db.exec 'drop database if exists pgvc_testing' }
+    .tap { |db| db.exec 'create database pgvc_testing' }
+  db = PG.connect dbname: 'pgvc_testing'
 
 # Create users and products
   db.exec <<~SQL
     SET client_min_messages=WARNING;
-
-    create table users (
-      id serial primary key,
-      name varchar
-    );
-    insert into users (name)
-      values ('josh'), ('lucy'), ('system');
-
     create table products (
       id serial primary key,
       name varchar,
       colour varchar
     );
-    insert into products (name, colour)
-      values ('boots', 'black');
   SQL
 
-# Load the library
+# Load the lib
   $LOAD_PATH.unshift File.expand_path('lib', __dir__)
-  require 'pgvc'
+  require 'pgvc/git'
+  Pgvc.init db, default_branch: 'master'
 
-# Pull our users out
-  josh, lucy, system =
-    db.exec("select * from users order by name;").map { |r| Pgvc::Record.new r }
-    # => [#<Record id="1" name="josh">,
-    #     #<Record id="2" name="lucy">,
-    #     #<Record id="3" name="system">]
+  git = Pgvc::Git.new db
 
-# Add version control to the database
-  pgvc = Pgvc.init db, system_user_ref: system.id, default_branch: 'master'
+# Some local work
+  git.exec "insert into products (name, colour) values ('boots', 'black')"
 
-# Josh is on the default branch, "master", which is in the "public" schema
-  master = pgvc.user_get_branch josh.id
-  # => #<Record id="1"
-  #             commit_hash="66d095e3c766f9648d691aabf061dc7c"
-  #             name="master"
-  #             schema_name="public"
-  #             is_default="t">
+# Initialize git
+  git.config_user_ref 'Josh Cheek'
+  git.init
 
-# Master is pointing at the initial commit, created by the system user
-  pgvc.get_commit master.commit_hash
-  # => #<Record vc_hash="66d095e3c766f9648d691aabf061dc7c"
-  #             db_hash=nil
-  #             user_ref="3"
-  #             summary="Initial commit"
-  #             description=""
-  #             created_at="2017-10-23 21:24:17.017444">
+# Add existing products
+  git.add_table 'products'
+  git.commit 'Add pre-existing products'
 
-# Tell pgvc to track the products table
-  pgvc.track_table 'products'
+# Check the log history
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add pre-existing products", "Josh Cheek"]]
 
-# Josh commits the boots
-  pgvc.create_commit summary: 'Add pre-existing products', user_ref: josh.id
+# 1 branch, master, which we are on
+  git.branch.map { |b| [b.name, b.current?] } # => [["master", true]]
 
-# Master has been updated to the new commit
-  master = pgvc.user_get_branch josh.id
-  commit = pgvc.get_commit master.commit_hash
-  # => #<Record vc_hash="ce1dbd22a6370a86f265973515efb4c4"
-  #             db_hash="fdad56fe1d8185215bb1da4441c5f5b2"
-  #             user_ref="1"
-  #             summary="Add pre-existing products"
-  #             description=""
-  #             created_at="2017-10-23 21:24:17">
+# Make a new branch and check it out
+  git.branch 'add-shoes'
+  git.branch.map { |b| [b.name, b.current?] } # => [["add-shoes", false], ["master", true]]
+  git.checkout 'add-shoes'
+  git.branch.map { |b| [b.name, b.current?] } # => [["add-shoes", true], ["master", false]]
 
-# Josh makes a new branch and updates the colour of the boots
-  pgvc.user_create_branch 'update-boots', josh.id
-  pgvc.switch_branch josh.id, 'update-boots'
-  pgvc.connection_for('update-boots').exec("update products set colour = 'brown'")
-  pgvc.connection_for('update-boots').exec('select * from products').to_a
-  # => [{"id"=>"1",
-  #      "name"=>"boots",
-  #      "colour"=>"brown",
-  #      "vc_hash"=>"9df1cc901a477daa1bc6f22b45225494"}]
+# Same history
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add pre-existing products", "Josh Cheek"]]
 
-# Lucy, still on the master branch, makes a new branch and adds shoes
-  pgvc.user_create_branch 'add-shoes', lucy.id
-  pgvc.switch_branch lucy.id, 'add-shoes'
-  pgvc.connection_for('add-shoes')
-      .exec("insert into products (name, colour) values ('shoes', 'white')")
-  pgvc.connection_for('add-shoes').exec('select * from products').to_a
-  # => [{"id"=>"1",
-  #      "name"=>"boots",
-  #      "colour"=>"black",
-  #      "vc_hash"=>"b95fcc685e439417cae418e97366685c"},
-  #     {"id"=>"2",
-  #      "name"=>"shoes",
-  #      "colour"=>"white",
-  #      "vc_hash"=>"4e87ce327c3d158a57dd9198bd010575"}]
+# Add some white shoes
+  git.exec "insert into products (name, colour) values ('shoes', 'white')"
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add pre-existing products", "Josh Cheek"]]
+  git.commit 'Add white shoes'
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add white shoes", "Josh Cheek"],
+  #     ["Add pre-existing products", "Josh Cheek"]]
 
-# And the master branch reflects neither of these changes
-  pgvc.connection_for('master').exec('select * from products').to_a
-  # => [{"id"=>"1",
-  #      "name"=>"boots",
-  #      "colour"=>"black",
-  #      "vc_hash"=>"b95fcc685e439417cae418e97366685c"}]
+# Products on our branch vs master
+  git.exec('select id, name, colour from products order by id')
+  # => [#<Record id="1" name="boots" colour="black">,
+  #     #<Record id="2" name="shoes" colour="white">]
+  git.checkout 'master'
+  git.exec('select id, name, colour from products order by id')
+  # => [#<Record id="1" name="boots" colour="black">]
 
-# Josh and Lucy both commit
-  update_boots = pgvc.create_commit summary: 'Boots are brown', description: '', user_ref: josh.id, created_at: Time.now
-  add_shoes    = pgvc.create_commit summary: 'Add shoes', description: '', user_ref: lucy.id, created_at: Time.now
-  pgvc.history_from update_boots.vc_hash # ~> NoMethodError: undefined method `history_from' for #<Pgvc:0x007f9da3842590>
-  # =>
-  pgvc.history_from add_shoes.vc_hash
-  # =>
+# Log from our branch vs master
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add pre-existing products", "Josh Cheek"]]
+  git.checkout 'add-shoes'
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add white shoes", "Josh Cheek"],
+  #     ["Add pre-existing products", "Josh Cheek"]]
 
-# Then, idk, merging or diffing or something
-
-# ~> NoMethodError
-# ~> undefined method `history_from' for #<Pgvc:0x007f9da3842590>
-# ~>
-# ~> /Users/xjxc322/code/pgvc/example.rb:81:in `<main>'
+# Fast-forward merge
+  git.checkout 'master'
+  git.merge 'add-shoes'
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add white shoes", "Josh Cheek"],
+  #     ["Add pre-existing products", "Josh Cheek"]]
+  git.exec('select * from products').map { |p| [p.id, p.name, p.colour] }
+  # => [["1", "boots", "black"], ["2", "shoes", "white"]]
--- example_git_style.rb
+++ example_git_style.rb
@@ -1,9 +1,9 @@
 # Reset the database
-require 'pg'
-PG.connect(dbname: 'postgres')
-  .tap { |db| db.exec 'drop database if exists pgvc_testing' }
-  .tap { |db| db.exec 'create database pgvc_testing' }
-db = PG.connect dbname: 'pgvc_testing'
+  require 'pg'
+  PG.connect(dbname: 'postgres')
+    .tap { |db| db.exec 'drop database if exists pgvc_testing' }
+    .tap { |db| db.exec 'create database pgvc_testing' }
+  db = PG.connect dbname: 'pgvc_testing'
 
 # Create users and products
   db.exec <<~SQL
@@ -16,67 +16,237 @@ db = PG.connect dbname: 'pgvc_testing'
   SQL
 
 # Load the lib
-$LOAD_PATH.unshift File.expand_path('lib', __dir__)
-require 'pgvc/git'
-Pgvc.init db, system_user_ref: 'system', default_branch: 'master'
+  $LOAD_PATH.unshift File.expand_path('lib', __dir__)
+  require 'pgvc/git'
+  Pgvc.init db, default_branch: 'master'
 
-git = Pgvc::Git.new db
+  $git = Pgvc::Git.new db
 
-# some local work
-git.exec "insert into products (name, colour) values ('boots', 'black')"
+  def git
+    $git
+  end
 
-# initialize git
-git.config_user_ref 'Josh Cheek'
-git.init
+# Helpers for the examples below, (for simplicity of the example, we aren't parameterising args, but don't do that for real :)
+  def add_products(products)
+    values = products.map { |k, v| "('#{k}', '#{v}')" }.join(', ')
+    git.exec "insert into products (name, colour) values #{values}"
+  end
+
+  def delete_products(attributes)
+    values = attributes.map { |name, value| "#{name} = '#{value}'" }.join(' and ')
+    git.exec "delete from products where #{values}"
+  end
+
+  def update_products(attribute, updates)
+    updates.each do |old, new|
+      git.exec_params "update products set #{attribute} = $1 where #{attribute} = $2", [new, old]
+    end
+  end
+
+  def show_products
+    git.exec 'select * from products'
+  end
+
+  def fancy_diff(*args)
+    git.exec("select * from git.diff(#{args.map { |a| "'#{a}'" }.join(", ")}) join vc.rows using (vc_hash) order by data")
+       .map { |p| [ p.action, p.table_name, eval("{#{p.data}}").map { |k, v| [k.intern, v] }.to_h ] }
+  end
+
+
+# Some relevant dates
+  ten_days_ago = (Date.today-10).strftime('%F')
+
+
+# Some local work
+  add_products "3d coyote"  => "gray", "coat"  => "navy",  "siding" => "white", "pipe" => "brass",
+               "power cord" => "gray", "relay" => "black", "wire"   => "blue"
+
+
+# Initialize git
+  # you can make this w/e you want, eg map it to your users table's primary key
+  git.config_user_ref 'system'
+  git.init
+
 
 # Add existing products
-git.add_table 'products'
-git.commit 'Add pre-existing products'
+  git.add_table 'products'
+
+
+# Normal git diff represents changes as hashes, the helper, fancy diff, shows us the values they correlate to
+  git.diff.map(&:to_a)
+  # => [["insert", "products", "46aa147d1c1c38ba011222c8d6d83b81"],
+  #     ["insert", "products", "6bfddf27a4390073b81328afc46b2b08"],
+  #     ["insert", "products", "764b1b449908d024c4eb7e8e9b6f8143"],
+  #     ["insert", "products", "a21736856686c10636db8e57c5a4322c"],
+  #     ["insert", "products", "b625b60c51d1e31d9b77fe96592bc168"],
+  #     ["insert", "products", "e5d8c394c05e17107f74b04420d63c9f"],
+  #     ["insert", "products", "e72da12427bddf4572900158f9220958"]]
+  fancy_diff
+  # => [["insert", "products", {:id=>"1", :name=>"3d coyote", :colour=>"gray"}],
+  #     ["insert", "products", {:id=>"2", :name=>"coat", :colour=>"navy"}],
+  #     ["insert", "products", {:id=>"3", :name=>"siding", :colour=>"white"}],
+  #     ["insert", "products", {:id=>"4", :name=>"pipe", :colour=>"brass"}],
+  #     ["insert", "products", {:id=>"5", :name=>"power cord", :colour=>"gray"}],
+  #     ["insert", "products", {:id=>"6", :name=>"relay", :colour=>"black"}],
+  #     ["insert", "products", {:id=>"7", :name=>"wire", :colour=>"blue"}]]
+  git.commit 'Add pre-existing products'
+  git.diff # => []
+
 
 # Check the log history
-git.log.map { |log| [log.summary, log.user_ref] }
-# => [["Add pre-existing products", "Josh Cheek"],
-#     ["Initial commit", "system"]]
+  git.log.map { |log| [log.summary, log.user_ref] }
+  # => [["Add pre-existing products", "system"]]
 
-# 1 branch, master, which we are on
-git.branch.map { |b| [b.name, b.current?] } # => [["master", true]]
 
-# make a new branch and check it out
-git.branch 'add-shoes'
-git.branch.map { |b| [b.name, b.current?] } # => [["add-shoes", false], ["master", true]]
-git.checkout 'add-shoes'
-git.branch.map { |b| [b.name, b.current?] } # => [["add-shoes", true], ["master", false]]
+# Publish this data and make a branch to track that (eventually introduce tags instead)
+  git.branch "publish #{ten_days_ago}"
 
-# Same history
-git.log.map { |log| [log.summary, log.user_ref] }
-# => [["Add pre-existing products", "Josh Cheek"],
-#     ["Initial commit", "system"]]
 
-# Add some white shoes
-git.exec "insert into products (name, colour) values ('shoes', 'white')"
-git.log.map { |log| [log.summary, log.user_ref] }
-# => [["Add pre-existing products", "Josh Cheek"],
-#     ["Initial commit", "system"]]
-git.commit 'Add white shoes'
-git.log.map { |log| [log.summary, log.user_ref] }
-# => [["Add white shoes", "Josh Cheek"],
-#     ["Add pre-existing products", "Josh Cheek"],
-#     ["Initial commit", "system"]]
+# Import footwear
+  git.config_user_ref 'Piet'
+  add_products shoes: "white", boots: 'black', sandals: 'brown'
+  git.commit 'Import footwear from supplier'
 
-# Products on our branch vs master
-git.exec('select id, name, colour from products order by id')
-# => [#<Record id="1" name="boots" colour="black">,
-#     #<Record id="2" name="shoes" colour="white">]
-git.checkout 'master'
-git.exec('select id, name, colour from products order by id')
-# => [#<Record id="1" name="boots" colour="black">]
 
-# Log from our branch vs master
-git.log.map { |log| [log.summary, log.user_ref] }
-# => [["Add pre-existing products", "Josh Cheek"],
-#     ["Initial commit", "system"]]
-git.checkout 'add-shoes'
-git.log.map { |log| [log.summary, log.user_ref] }
-# => [["Add white shoes", "Josh Cheek"],
-#     ["Add pre-existing products", "Josh Cheek"],
-#     ["Initial commit", "system"]]
+# No uncommitted changes, but we can see them if we diff from the last publish
+  fancy_diff # => []
+  fancy_diff "publish #{ten_days_ago}"
+  # => [["insert", "products", {:id=>"10", :name=>"sandals", :colour=>"brown"}],
+  #     ["insert", "products", {:id=>"8", :name=>"shoes", :colour=>"white"}],
+  #     ["insert", "products", {:id=>"9", :name=>"boots", :colour=>"black"}]]
+
+
+# Maggie realizes that the 3D Coyote is actually a 2D Coyote
+  git.config_user_ref 'Maggie'
+  update_products :name, '3d coyote' => '2d coyote'
+  show_products.map(&:name)
+  git.commit 'Fix "2d coyote", which was mislabeled as "3d coyote"'
+
+
+# Another publish
+  git.branch "publish #{(Date.today-5).strftime('%F')}"
+
+
+# Delete boots
+  git.config_user_ref 'System'
+  delete_products name: 'coat'
+  git.commit 'Delete products removed from feed'
+
+
+# Another publish
+  git.branch "publish #{(Date.today-4).strftime('%F')}"
+
+
+# Check the log
+  git.log.map { |l| [l.user_ref, l.summary] }
+  # => [["System", "Delete products removed from feed"],
+  #     ["Maggie", "Fix \"2d coyote\", which was mislabeled as \"3d coyote\""],
+  #     ["Piet", "Import footwear from supplier"],
+  #     ["system", "Add pre-existing products"]]
+
+
+# Check the branches
+  git.branch.map { |b| [b.current?, b.name] }
+  # => [[true, "master"],
+  #     [false, "publish 2017-10-21"],
+  #     [false, "publish 2017-10-26"],
+  #     [false, "publish 2017-10-27"]]
+
+
+# Now Gordana starts a long running change, lets do it on a branch
+  git.config_user_ref 'Gordana'
+  git.branch 'Electrical Review'
+  git.checkout 'Electrical Review'
+  git.branch.map { |b| [b.current?, b.name] }
+  # => [[true, "Electrical Review"],
+  #     [false, "master"],
+  #     [false, "publish 2017-10-21"],
+  #     [false, "publish 2017-10-26"],
+  #     [false, "publish 2017-10-27"]]
+
+
+# Gordana updates the names to be capitalized
+  update_products :name, 'power cord' => 'Power Cord'
+  fancy_diff
+  # => [["insert", "products", {:id=>"5", :name=>"Power Cord", :colour=>"gray"}],
+  #     ["delete", "products", {:id=>"5", :name=>"power cord", :colour=>"gray"}]]
+  git.commit 'Electrical Review: Capitalize name for "Power cord"'
+
+
+# How is that different from what is on the master branch?
+  fancy_diff 'master'
+  # => [["insert", "products", {:id=>"5", :name=>"Power Cord", :colour=>"gray"}],
+  #     ["delete", "products", {:id=>"5", :name=>"power cord", :colour=>"gray"}]]
+
+
+# Meanwhile, Ji-Sook wants to do a change for ICM, she's on the "master" branch and can't see Gordana's changes
+  git.config_user_ref 'Ji-Sook'
+  git.branch.map { |b| [b.current?, b.name] }
+  # => [[false, "Electrical Review"],
+  #     [true, "master"],
+  #     [false, "publish 2017-10-21"],
+  #     [false, "publish 2017-10-26"],
+  #     [false, "publish 2017-10-27"]]
+
+
+# She checks out the branch and updates the boots from brown to tan
+  git.branch 'ICM-997'
+  git.checkout 'ICM-997'
+  update_products :colour, brown: 'tan'
+  fancy_diff
+  # => [["delete", "products", {:id=>"10", :name=>"sandals", :colour=>"brown"}],
+  #     ["insert", "products", {:id=>"10", :name=>"sandals", :colour=>"tan"}]]
+  git.commit 'ICM-997 fix colour of the sandals'
+
+
+# She doesn't see Gordana's changes because she branched off of "master", not "Electrical Review"
+  git.log.map { |l| [l.user_ref, l.summary] }
+  # => [["Ji-Sook", "ICM-997 fix colour of the sandals"],
+  #     ["System", "Delete products removed from feed"],
+  #     ["Maggie", "Fix \"2d coyote\", which was mislabeled as \"3d coyote\""],
+  #     ["Piet", "Import footwear from supplier"],
+  #     ["system", "Add pre-existing products"]]
+
+
+# What has she changed as compared to what is published?
+  fancy_diff 'master'
+  # => [["delete", "products", {:id=>"10", :name=>"sandals", :colour=>"brown"}],
+  #     ["insert", "products", {:id=>"10", :name=>"sandals", :colour=>"tan"}]]
+
+
+# What has she changed as compared to Gordana's "Electrical Review"?
+  fancy_diff 'Electrical Review'
+  # => [["delete", "products", {:id=>"10", :name=>"sandals", :colour=>"brown"}],
+  #     ["insert", "products", {:id=>"10", :name=>"sandals", :colour=>"tan"}],
+  #     ["delete", "products", {:id=>"5", :name=>"Power Cord", :colour=>"gray"}],
+  #     ["insert", "products", {:id=>"5", :name=>"power cord", :colour=>"gray"}]]
+
+
+# Ji-Sook merges her change into the master branch
+  git.checkout 'master'
+  git.log.map { |l| [l.user_ref, l.summary] }
+  # => [["System", "Delete products removed from feed"],
+  #     ["Maggie", "Fix \"2d coyote\", which was mislabeled as \"3d coyote\""],
+  #     ["Piet", "Import footwear from supplier"],
+  #     ["system", "Add pre-existing products"]]
+
+
+
+# ...AAAAAND THIS IS HOW FAR WE CURRENTLY ARE :P
+  git.merge 'ICM-997' # ~> PG::UniqueViolation: ERROR:  duplicate key value violates unique constraint "products_pkey"\nDETAIL:  Key (id)=(10) already exists.\nCONTEXT:  SQL statement " insert into products select vc_record.*\n               from populate_record(null::products, $1) as vc_record\n            "\nPL/pgSQL function git.merge(character varying) line 31 at EXECUTE\n
+  git.log.map { |l| [l.user_ref, l.summary] }
+  # =>
+
+# ~> PG::UniqueViolation
+# ~> ERROR:  duplicate key value violates unique constraint "products_pkey"
+# ~> DETAIL:  Key (id)=(10) already exists.
+# ~> CONTEXT:  SQL statement " insert into products select vc_record.*
+# ~>                from populate_record(null::products, $1) as vc_record
+# ~>             "
+# ~> PL/pgSQL function git.merge(character varying) line 31 at EXECUTE
+# ~>
+# ~> /Users/xjxc322/code/pgvc/lib/pgvc/git.rb:60:in `exec_params'
+# ~> /Users/xjxc322/code/pgvc/lib/pgvc/git.rb:60:in `exec_params'
+# ~> /Users/xjxc322/code/pgvc/lib/pgvc/git.rb:48:in `fn'
+# ~> /Users/xjxc322/code/pgvc/lib/pgvc/git.rb:42:in `merge'
+# ~> /Users/xjxc322/code/pgvc/example_git_style.rb:193:in `<main>'
